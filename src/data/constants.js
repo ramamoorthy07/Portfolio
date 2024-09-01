ef18b7546218b9fd95a0d65fa5d7bfd788e37f9d
Ramamoorthy S <69966109+ramamoorthy07@users.noreply.github.com>
2024-09-02 02:57:14 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,269 @@
+export const Bio = {
+    name: "Ramamoorthy S",
+    roles: [
+      "AI Engineer",
+      "LLM Specialist",
+      "Cloud Engineer",
+      "Python Developer",
+    ],
+    description:
+      "Dedicated AI Engineer with 1 year of Python experience. Skilled in designing and deploying ML models, developing data-driven apps, and leveraging cutting-edge tech for innovation. Thrives in fast-paced environments, focused on delivering high-quality, scalable solutions.",
+    github: "https://github.com/ramamoorthy07",
+    resume:
+      "https://drive.google.com/file/d/1P44VXvkbNie5koeyAv187Bt6YYZUj1sq/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/ramamoorthy07/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "AWS",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/images%20(1).png?alt=media&token=659571ee-2148-46f5-9382-3a477de9d92a",
+        },
+        {
+          name: "Neo4j",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/neo4j-FB.jpg?alt=media&token=3a2c731c-95f3-491d-9f03-57ccd3c0736f",
+        },
+
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/git6963.jpg?alt=media&token=c2d241d5-2e8e-4bfc-b663-e610f41083ab",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+      ],
+    },
+    {
+      title: "AI and Machine Learning",
+      skills: [
+        {
+          name: "TensorFlow",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/FtFnqC38_400x400.png?alt=media&token=23f3477f-e05d-494b-9d87-b216fb96ad88",
+        },
+        {
+          name: "NLP",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/what_is_NLP.jpg?alt=media&token=9ef92732-5f99-4f1f-892b-ff1dd2e5dc03",
+        },
+        {
+          name: "RAG",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/images%20(2).png?alt=media&token=2fe14c21-519f-4105-a13b-01a424b261a4",
+        },
+      ],
+    },
+    {
+      title: "Language Models and Conversational AI",
+      skills: [
+        {
+          name: "LangChain",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/1_44fD_VXcqw2kDWublQLONw.jpg?alt=media&token=8b78abeb-ad13-4645-ac1b-b7e8cd6bd0cb",
+        },
+        {
+          name: "LangGraph",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/1_pvsQBbW2fTYhvYcQIMCt3w.png?alt=media&token=5194a62d-64ee-461e-ada9-004e9a969084",
+        },
+        {
+          name: "CrewAI",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/download.jpg?alt=media&token=00b521cb-8cba-4783-a56c-fcf87f5036bc",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/unnamed.png?alt=media&token=3340e033-389e-4ca1-9d0d-eb363241b15a",
+      role: "Freelancer - AI Engineer",
+      date: "July 2024 - present",
+      desc: "Working on OS Platforms, building LLM Applications , Deploying in the cloud services and making documentations.",
+      skills: [
+        "TensorFlow",
+        "PyTorch",
+        "Keras",
+        "Pandas",
+        "Matplotlib",
+        "THugging Face",
+        "OpenAI's GPT",
+        "AWS",
+        "Flask",
+        "Git",
+        "SQL",
+        "GrapRAG",
+        "LangChain"
+      ],
+    },
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/Databels%20logo.jpg?alt=media&token=3756b866-8e68-4f41-8af2-f20ef387d57e",
+      role: "Python Developer",
+      company: "Databels Solutions Pvt. Ltd.",
+      date: "June 2023 - June 2024",
+      desc:  "Python Developer with 1 year of experience in building and optimizing applications. Skilled in Python and AI/ML technologies. Proven problem-solver with a focus on innovative solutions.",
+      skills: [
+        "Python",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/College%20Logo.jpg?alt=media&token=3376d207-6397-47a6-b12e-f7ff28015290",
+      school: "K.Ramakrishnan College of Technology,Trichirappalli,Tamilnadu.",
+      date: "June 2019 - May 2024",
+      grade: "8.23 CGPA",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/1692343823586.jpg?alt=media&token=aa7407e9-942f-4184-9e93-0db8ad3bfa44",
+      school : "Campion Anglo-Indian Higher Secondary School,Tiruchirappalli,Tamilnadu",
+      date: "June 2017 - April 2019",
+      degree: "Computer Science",
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/images.jpg?alt=media&token=64b3410d-161e-41a5-9338-654c143a0b6e",
+      school: "R.C Higher Secondary School,Tiruchirappalli,Tamilnadu",
+      date: "June 2012 - April 2017",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "City 360",
+      date: "April 2024",
+      description:
+        "The Trichy City Services Finder is an application designed to provide users with comprehensive information about various services and locations within the city of Trichy. By leveraging AI agents and specialized tasks, the application can find and display details about holy places, shopping marts, courier services, restaurants, service centers, home appliances stores, entertainment options, and many more. This tool aims to make it easy for users to locate and explore different services efficiently.",
+      image:
+        "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+      tags: [
+        "CrewAI",
+        "Python",
+        "Data Management",
+        "APIs",
+        "AWS"
+      ],
+      category: "web app",
+      github: "https://github.com/ramamoorthy07/CrewAI-City-360",
+    },
+    
+    {
+      id: 3,
+      title: "AI-Chatbot-Application",
+      date: "June 2024",
+      description:
+        "Chatbot Development with Microsoft DialoGPT and Flask In this project, I developed an advanced chatbot that engages users in natural, conversational interactions. The chatbot is powered by Microsoft DialoGPT, a pre-trained language model known for generating human-like responses.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/Screenshot%202024-09-02%20004700.png?alt=media&token=4df87b0e-fc55-4fb9-9386-9821d469bac4",
+      tags: ["Microsoft DialoGPT", "Flask", "HTML", "CSS", "JavaScript", "jQuery"],
+      category: "machine learning",
+      github: "https://github.com/ramamoorthy07/AI-Chatbot-Application.git",
+    },
+    {
+      id: 0,
+      title: "An Efficient Spam Detection Techniques For IOT Devices Using Machine Learning",
+      date: "Jan 2023 - May 2023",
+      description:
+        "From developing the efficient spam detection techniques for IoT devices, adept at designing and implementing machine learning algorithms and deep learning models to analyze IoT network traffic and identify malicious behavior. Skilled in data preprocessing, feature engineering, and model optimization, with a strong understanding of cybersecurity principles and IoT protocols. Collaborative team player with excellent communication skills and a commitment to staying updated with the latest trends in machine learning and IoT security.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/Screenshot%202024-09-02%20011633.png?alt=media&token=9adfa9a1-6b37-4b58-b48d-1ec4f3dd0524",
+      tags: ["Python", "Scikit-Learn", "XGBoost/LightGBM", "Pandas", "MQTT"],
+      category: "web app",
+
+    },
+    {
+      id: 1,
+      title: "Face Mask Object Detection Using Machine Learning",
+      date: "Oct 2021",
+      description:
+        "Created a Face mask detection+recognition mobile application by passing the face which detected using Google Face Detection API into Image Classification model. When no mask identified then face recognition will be performed using MobileFaceNET architecture and inference will be sent to Text-to-Speech API for audio feedback",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-b9ce3.appspot.com/o/Face%20Mask%20Detection.jpeg?alt=media&token=8e4f8d80-05a6-42c0-823b-66f1b308b25c",
+      tags: [
+        "Python",
+        "TensorFlow/Keras",
+        "OpenCV",
+        "YOLO",
+        "LabelImg",
+        "Data Augmentation Libraries",
+      ],
+      category: "machine Learning",
+    },
+    
+  ];
+  
+ 
